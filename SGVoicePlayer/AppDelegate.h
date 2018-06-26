cba809e82ef78416271345103bbf541e2067dfc7
Aban <aban@Macs-iMac.local>
2018-06-26 21:34:08 +0430

    nothing

---
+++ SGVoicePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SGVoicePlayer
+//
+//  Created by Aban on 6/26/18.
+//  Copyright © 2018 Aban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SGVoicePlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SGVoicePlayer
+//
+//  Created by Aban on 6/26/18.
+//  Copyright © 2018 Aban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
